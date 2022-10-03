138a7bb3b6e7983859ae8e2726a7e0fccd555900
Jiu_Mao <79178880+Jiu-Mao@users.noreply.github.com>
2022-10-03 21:58:54 +0800

    123

--- Scheduler.cpp
+++ Scheduler.cpp
@@ -3,14 +3,17 @@
 
 Scheduler::Scheduler(QWidget *parent) : QWidget(parent)
 {
+    QPushButton *button1 = new QPushButton("1");
+    QPushButton *button2 = new QPushButton("2");
+
     QHBoxLayout *hBoxLayout = new QHBoxLayout;
 
-    QPushButton *button1 = new QPushButton(tr("1"));
-
     hBoxLayout->addWidget(button1, 0);
+    hBoxLayout->addWidget(button2, 1);
 
-    QGridLayout *mainLayout = new QGridLayout(this);
-    mainLayout->addLayout(hBoxLayout,0,0);
+    QGridLayout *gridLayout = new QGridLayout;
+    gridLayout->addLayout(hBoxLayout, 0, 0);
 
+    setLayout(gridLayout);
     setWindowTitle(tr("Scheduler"));
 }
